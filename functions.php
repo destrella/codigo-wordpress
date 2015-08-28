d63b54e13e89bfe8284d1711fe7d4f6a262e3929
Daniel Estrella <destrella@users.noreply.github.com>
2015-08-28 10:45:02 -0500

    Desactiva emojis
    Agregada función para desactivar los scripts y css de emojis

--- functions.php
+++ functions.php
@@ -200,6 +200,20 @@ function columnas_personalizadas($columna, $id)
             break;
     endswitch;
 }
+/********************************************************************/
+/** Desactiva scripts y estilos de emojis.                         **/
+/********************************************************************/
+function no_emojis()
+{
+	remove_action('wp_head', 'print_emoji_detection_script', 7);
+	remove_action('admin_print_scripts', 'print_emoji_detection_script');
+	remove_action('wp_print_styles', 'print_emoji_styles');
+	remove_action('admin_print_styles', 'print_emoji_styles');
+	remove_filter('the_content_feed', 'wp_staticize_emoji');
+	remove_filter('comment_text_rss', 'wp_staticize_emoji');
+	remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+}
+add_action('init', 'no_emojis');
 
 /********************************************************************/
 /** Combina archivos CSS                                           **/
